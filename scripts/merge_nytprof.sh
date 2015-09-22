39ff70fd0e574915050f305d6bb66153576b8e08
y_uuki <yuki.tsubo@gmail.com>
2015-09-23 01:42:45 +0900

    missing carton exec

--- scripts/merge_nytprof.sh
+++ scripts/merge_nytprof.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nytprofmerge --out /tmp/nytprof-merged.out /tmp/nytprof.out.*
-nytprofhtml --minimal --delete --file /tmp/nytprof-merged.out
+carton exec nytprofmerge --out /tmp/nytprof-merged.out /tmp/nytprof.out.*
+carton exec nytprofhtml --minimal --delete --file /tmp/nytprof-merged.out
