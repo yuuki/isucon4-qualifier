5575adf68e5784798fc20ba4b13d7db9273a5c8c
y_uuki <yuki.tsubo@gmail.com>
2015-09-23 02:05:32 +0900

    nytprof_reportの出力パスを指定

--- scripts/merge_nytprof.sh
+++ scripts/merge_nytprof.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 carton exec nytprofmerge --out /tmp/nytprof-merged.out /tmp/nytprof.out.*
-carton exec nytprofhtml --minimal --delete --file /tmp/nytprof-merged.out
+carton exec nytprofhtml --minimal --delete --file /tmp/nytprof-merged.out -o /tmp/nytprof_report
