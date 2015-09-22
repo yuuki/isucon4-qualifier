bd2bb2c39cd35487802a4e1326ac324e3e24c698
y_uuki <yuki.tsubo@gmail.com>
2015-09-23 01:24:56 +0900

    chmod +x

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 NYTPROF="sigexit=int:savesrc=0:start=no:addpid=1:forkdepth=0:file=/tmp/nytprof.out"
-exec carton exec plackup -d:NYTProf -s Starman --host localhost:8080 -E prod app.psgi
+exec carton exec perl -d:NYTProf local/bin/plackup -s Starman --host localhost:8080 -E prod app.psgi
