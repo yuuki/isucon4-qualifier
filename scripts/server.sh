9ae32480cdd1c5aa356163a2e59bc36487c1b656
y_uuki <yuki.tsubo@gmail.com>
2015-09-23 02:30:47 +0900

    古いnytprofのデータを参照しないように

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export NYTPROF="sigexit=int:savesrc=0:start=no:addpid=1:forkdepth=0:file=/tmp/nytprof.out"
+export NYTPROF="sigexit=int:start=no:addpid=1:forkdepth=0:file=/tmp/nytprof.out"
 exec carton exec perl -d:NYTProf local/bin/plackup -s Starman --host localhost:8080 -E prod app.psgi
