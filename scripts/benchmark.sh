1a94426c678de1b6219de60d834035325689be63
y_uuki <yuki.tsubo@gmail.com>
2015-09-22 18:03:32 +0900

    Fix typo

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -7,4 +7,4 @@ WORKLOAD_NUM=$1
 sudo service mysql restart
 sudo rm -f /var/log/nginx/isucon4.access_log.tsv /var/log/nginx/access.log
 sudo service nginx restart
-/home/isucon/benchmark bench --workload $WORKLOAD_NUM
+/home/isucon/benchmarker bench --workload $WORKLOAD_NUM
