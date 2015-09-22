f546e918da6eeff90ebccac6e2c8f2864b0d27de
y_uuki <yuki.tsubo@gmail.com>
2015-09-23 01:33:35 +0900

    add supervisorctl init to benchmark.sh

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -7,4 +7,7 @@ WORKLOAD_NUM=$1
 sudo service mysql restart
 sudo rm -f /var/log/nginx/isucon4.access_log.tsv /var/log/nginx/access.log
 sudo service nginx restart
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl restart isucon_perl
 /home/isucon/benchmarker bench --workload $WORKLOAD_NUM
