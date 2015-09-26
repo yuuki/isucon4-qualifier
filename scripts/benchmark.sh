d66d43ffe089e535f4d4a167d90cdaba2379a3e4
y_uuki <yuki.tsubo@gmail.com>
2015-09-26 18:45:07 +0900

    mysqlの再起動より前にslowlogを削除

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -4,10 +4,10 @@ set -ex
 
 WORKLOAD_NUM=$1
 
-sudo service mysql restart
-sudo rm -f /var/log/nginx/isucon4.access_log.tsv /var/log/nginx/access.log
 sudo rm -f /var/lib/mysql/mysqld-slow.log
+sudo rm -f /var/log/nginx/isucon4.access_log.tsv /var/log/nginx/access.log
 sudo rm -fr /tmp/nytprof.out.* /tmp/nytprof_report
+sudo service mysql restart
 sudo service nginx restart
 sudo supervisorctl reread
 sudo supervisorctl update
