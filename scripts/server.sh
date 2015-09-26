0df456b6bb229bc620fd8c6e968d66320a1f79b6
y_uuki <yuki.tsubo@gmail.com>
2015-09-26 21:43:46 +0900

    off nytprof

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #export NYTPROF="sigexit=int:start=no:addpid=1:forkdepth=0:file=/tmp/nytprof.out"
-exec carton exec start_server --path /dev/shm/app.sock --backlog 16384 -- perl -d:NYTProf local/bin/plackup -s Gazelle --host localhost:8080 --max-reqs-per-child 50000 -workers=4 -E prod app.psgi
+exec carton exec start_server --path /dev/shm/app.sock --backlog 16384 -- perl local/bin/plackup -s Gazelle --host localhost:8080 --max-reqs-per-child 50000 -workers=4 -E prod app.psgi
