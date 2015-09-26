44d08e5d054671b65212ffaaf27ca0bf90985e15
y_uuki <yuki.tsubo@gmail.com>
2015-09-26 15:01:50 +0900

    slow log を毎回消すように

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -6,6 +6,7 @@ WORKLOAD_NUM=$1
 
 sudo service mysql restart
 sudo rm -f /var/log/nginx/isucon4.access_log.tsv /var/log/nginx/access.log
+sudo rm -f /var/lib/mysql/mysqld-slow.log
 sudo rm -fr /tmp/nytprof.out.* /tmp/nytprof_report
 sudo service nginx restart
 sudo supervisorctl reread
