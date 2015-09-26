897b323eac8d0a18e99e5c47f7b1e39ee66d6258
y_uuki <yuki.tsubo@gmail.com>
2015-09-26 14:59:27 +0900

    login_log に index はる

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,6 +8,6 @@ mydb=isu4_qualifier
 myhost=127.0.0.1
 myport=3306
 mysql -h ${myhost} -P ${myport} -u ${myuser} -e "DROP DATABASE IF EXISTS ${mydb}; CREATE DATABASE ${mydb}"
-mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/schema.sql
+mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < /home/isucon/deploy/sql/schema.sql
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_users.sql
 mysql -h ${myhost} -P ${myport} -u ${myuser} ${mydb} < sql/dummy_log.sql
