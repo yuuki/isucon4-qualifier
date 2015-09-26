d164caa7cea724b562e46f0cf776e8c7359fa1fe
y_uuki <yuki.tsubo@gmail.com>
2015-09-26 19:28:53 +0900

    Gazelle param

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export NYTPROF="sigexit=int:start=no:addpid=1:forkdepth=0:file=/tmp/nytprof.out"
-exec carton exec perl -d:NYTProf local/bin/plackup -s Gazelle --host localhost:8080 -E prod app.psgi
+exec carton exec perl -d:NYTProf local/bin/plackup -s Gazelle --host localhost:8080 --max-reqs-per-child 50000 -workers=4 -E prod app.psgi
